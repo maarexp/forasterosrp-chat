cf34bba8c4b094250a305d26cc7682cf79e675f4
Edu Martínez <50999100+marexp99@users.noreply.github.com>
2020-01-17 17:20:39 +0100

    carga de los mensajes en cloud
    se cargan los mensajes en la base de datos pero aun no consigo verlos en la app

--- lib/main.dart
+++ lib/main.dart
@@ -277,6 +277,22 @@ class _ChatState extends State<Chat> {
   TextEditingController messageController = TextEditingController();
   ScrollController scrollController = ScrollController();
 
+  Future<void> callback() async {
+    if (messageController.text.length > 0) {
+      await
+      _firestore.collection('message').add({
+        'text': messageController.text,
+        'from': widget.user.email,
+      });
+      messageController.clear();
+      scrollController.animateTo(
+        scrollController.position.maxScrollExtent,
+        curve: Curves.easeOut,
+        duration: const Duration(milliseconds: 300),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -287,9 +303,7 @@ class _ChatState extends State<Chat> {
             height: 40.0,
             child: Image.asset("assets/image/logo.png"),
           ),
-
         ),
-
         title: Text("ForasterosRP Chat"),
         actions: <Widget>[
           IconButton(
@@ -307,12 +321,104 @@ class _ChatState extends State<Chat> {
           children: <Widget>[
             Expanded(
               child: StreamBuilder<QuerySnapshot>(
-                stream: _firestore.collection('mkessages').snapshots(),
-                builder: (context, snapshot),
-              )
-            )
+                stream: _firestore.collection('messages').snapshots(),
+                builder: (context,snapshot) {
+                  if (!snapshot.hasData)
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+
+                  List<DocumentSnapshot> docs = snapshot.data.documents;
+
+                  List<Widget> messages = docs.map((doc) => Message(
+                    from: doc.data['from'],
+                    text: doc.data['text'],
+                    me: widget.user.email == doc.data['from'],
+                  ))
+                  .toList();
+
+                  return ListView(
+                    controller: scrollController,
+                    children: <Widget>[
+                      ... messages,
+                    ],
+                  );
+                },
+              ),
+            ),
+           Container(
+             child: Row(
+               children: <Widget>[
+                 Expanded(
+                   child: TextField(
+                     onSubmitted: (value) => callback(),
+                     decoration: InputDecoration(
+                       hintText: "Escribe un Mensaje...",
+                       border: const OutlineInputBorder(),
+                     ),
+                     controller: messageController,
+                   ),
+                 ),
+                 SendButton(
+                   text: "Enviar",
+                   callback: callback,
+                 )
+               ],
+             ),
+           ),
+
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SendButton extends StatelessWidget {
+  final String text;
+  final VoidCallback callback;
+
+  const SendButton({Key key, this.text, this.callback}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      color: Colors.orange,
+      onPressed: callback,
+      child: Text(text),
+    );
+  }
+}
+
+class Message extends StatelessWidget {
+  final String from;
+  final String text;
+
+  final bool me;
+
+  const Message({Key key, this.from, this.text, this.me}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: me ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            from,
+          ),
+          Material(
+            color: me ? Colors.teal : Colors.red,
+            borderRadius: BorderRadius.circular(10.0),
+            elevation: 6.0,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+              child: Text(
+                text,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
